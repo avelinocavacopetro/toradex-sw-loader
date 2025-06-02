e987ad82a3c734e4ec11532011ddccbc8868a05b
JoaOliveira04 <joao.oliveira@petrotec.com>
2025-06-02 15:20:17 +0100

    Registo das atividades de seleção de dispositivos e software na base de dados

---
+++ ToradexSwLoader/Models/UserActivity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToradexSwLoader.Models
+{
+    [Table("UserActivity")]
+    public class UserActivity
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string ActMessage { get; set; }
+
+        [Required]
+        public DateTime ActTimeStamp { get; set; }
+    }
+}
