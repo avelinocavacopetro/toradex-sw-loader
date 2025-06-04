aea52b6e7f5b01235cc8ac2cd07d1de24d8e9686
JoaOliveira04 <joao.oliveira@petrotec.com>
2025-06-04 13:38:00 +0100

    Username à mostra

--- ToradexSwLoader/Services/LoginService.cs
+++ ToradexSwLoader/Services/LoginService.cs
@@ -5,11 +5,15 @@
         public int CurrentLoginId { get; private set; }
         public string Email { get; private set; }
 
+        public event Action? OnLoginChanged;
+
         public void SetLogin(int loginId, string email)
         {
             CurrentLoginId = loginId;
             Email = email;
+            OnLoginChanged?.Invoke();
         }
     }
+
 
 }
