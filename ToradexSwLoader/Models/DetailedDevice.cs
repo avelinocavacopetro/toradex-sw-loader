0ff0a65df048ee9c58ad6b0f0c8bf9848f180aa6
JoaOliveira04 <joao.oliveira@petrotec.com>
2025-06-24 08:55:02 +0100

    Melhorias no codigo

---
+++ ToradexSwLoader/Models/DetailedDevice.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToradexSwLoader.Models
+{
+    [Table("DetailedDevice")]
+    public class DetailedDevice
+    {
+        [Key]
+        public string DeviceUuid { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string DeviceName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
+        public string DeviceId { get; set; } = null!;
+
+        public DateTime? LastSeen { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? ActivatedAt { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string DeviceStatus { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string? Notes { get; set; }
+
+        public bool Hibernated { get; set; }
+    }
+}
