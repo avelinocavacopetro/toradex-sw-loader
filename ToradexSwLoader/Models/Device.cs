376b6ae0c7a4db19c4e5404d3ab47747d2068b16
JoaOliveira04 <joao.oliveira@petrotec.com>
2025-06-30 09:24:45 +0100

    CRUD que faltava na gestão de dispositivos

--- ToradexSwLoader/Models/Device.cs
+++ ToradexSwLoader/Models/Device.cs
@@ -9,7 +9,7 @@ namespace ToradexSwLoader.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-
+        public bool Enabled { get; set; } 
         public ICollection<DeviceProduct> DeviceProducts { get; set; } = new List<DeviceProduct>();
     }
 }
