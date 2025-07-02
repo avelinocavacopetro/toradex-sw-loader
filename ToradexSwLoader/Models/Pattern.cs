df772813a99d63b9a4cf84e05157b23844aa524a
JoaOliveira04 <joao.oliveira@petrotec.com>
2025-07-02 08:40:28 +0100

    Adição de placa no final do wizard

---
+++ ToradexSwLoader/Models/Pattern.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToradexSwLoader.Models
+{
+    [Table("Pattern")]
+    public class Pattern
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string NameContains { get; set; } = string.Empty;
+    }
+}
