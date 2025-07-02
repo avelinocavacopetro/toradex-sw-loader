2d3adc26279a2aebb6c76c386127a5bc77b78991
tatianapatrica04 <tatiana.patricia@petrotec.com>
2025-07-02 08:41:47 +0100

    Mudança de Produto para Stack

---
+++ ToradexSwLoader/Models/StackPackage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToradexSwLoader.Models
+{
+    [Table("StackPackage")]
+    public class StackPackage
+    {
+        [ForeignKey("Stack")]
+        public int StackId { get; set; }
+        public Stack? Stack { get; set; }
+
+        [ForeignKey("Package")]
+        public string PackageId { get; set; }
+        public Package? Package { get; set; }
+    }
+}
