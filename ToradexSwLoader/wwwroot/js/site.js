9fa31aff491e0145390c690f5af44442e2eed654
tatianapatrica04 <tatiana.patricia@petrotec.com>
2025-08-13 14:50:45 +0100

    Tabela dos dispositivos acompanha ecrã e exportação para excel

--- ToradexSwLoader/wwwroot/js/site.js
+++ ToradexSwLoader/wwwroot/js/site.js
@@ -10,3 +10,12 @@ window.registerResizeCallback = (dotNetHelper) => {
         dotNetHelper.invokeMethodAsync("OnResize");
     });
 };
+
+function downloadFile(fileName, fileContent) {
+    var link = document.createElement('a');
+    link.download = fileName;
+    link.href = "data:application/octet-stream;base64," + fileContent;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
